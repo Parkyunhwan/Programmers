22560289f402c0be6f1010a1a8e72c04bfdbd169
Yunhwan Park <pyh8618@gmail.com>
2020-09-07 03:35:00 +0900

    200907 Add 문자열내맘대로정렬하기

--- Level_1/문자열내맘대로정렬하기.cpp
+++ Level_1/문자열내맘대로정렬하기.cpp
@@ -1,7 +1,27 @@
+# version 1
+## 먼저 전체 요소 소팅 후 특정 인덱스로 소팅
 def solution(strings, n):
-    answer = []
-    for string in strings:
-        answer.append(string[n] + string)
-    answer.sort()
-    answer = [x[1:] for x in answer]
-    return answer
+    def sortkey(x):
+        return x[n]
+    strings.sort()
+    strings.sort(key=sortkey)
+    return strings
+
+# version 2
+## sort의 key를 사용하지 않았을 경우
+
+def solution(strings, n):
+    for i in range(len(strings)):
+        strings[i] = string[i][n] + strings[i]
+    strings.sort() # 이를 통해 두 번의 소팅을 한번에 해결할 수 있다.    
+    
+    # 앞의 문자를 빼줘야함
+    for i in range(len(strings)):
+        strings[i] = strings[i][1:]
+    return strings
+
+# version 3
+## lamda 를 사용한 해결법
+
+def solution(strings, n):
+    return sorted( sorted(strings), key=lambda x : x[n])
